8f36a9d5afe7b0ebfaa6b99ee60b514d4cc9bfa6
faizm10 <faizmustansar10@gmail.com>
2024-05-21 11:23:52 -0400

    init

---
+++ website/src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="bg-blue-400 flex justify-center items-center">
+      <div className="text-center">
+        <img src="/images/tashfeen.jpeg" alt="Profile Picture" className="w-62 h-62 rounded-full mx-auto border-4 border-gray-300" />
+        <h1 className="text-4xl font-bold mt-4">Tashfeen Karamat</h1>
+        <p className="text-xl">Associate Professor @ Carleton University</p>
+      </div>
+    </div>
+  )
+}
